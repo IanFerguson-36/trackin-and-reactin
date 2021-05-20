fce8e1cc472a5a68c186b3a00a1d889ef04fdd93
Ian F <ian.ferg36@gmail.com>
2021-05-19 18:26:45 -0600

    created stylesheet and connected it to expense item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,14 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item</h2>;
+  return (
+    <div className="expense-item">
+      <div>May 19 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Inusrance</h2>
+        <div className="expense-item__price">$69</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
